8be65c204d78be2fcd3c5d4e31576bb4dd4e0e21
Devon Wintz <samuelwintz@gmail.com>
2020-09-15 14:48:55 -0400

    updated project image & resume

---
+++ docs/precache-manifest.8179dc81b2bbca29cedae6405aca9ddd.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ffcd566dffdc1973b5e3ab8f7a3eab5",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "057275813b66854683e3",
+    "url": "/my-portfolio/static/css/main.209e41f1.chunk.css"
+  },
+  {
+    "revision": "3c25e8c599ecbe79b176",
+    "url": "/my-portfolio/static/js/2.9ee42b00.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/my-portfolio/static/js/2.9ee42b00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "057275813b66854683e3",
+    "url": "/my-portfolio/static/js/main.77765b24.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "0aaca9454f75279610d6094f2b297241",
+    "url": "/my-portfolio/static/media/Coming Soon.0aaca945.png"
+  },
+  {
+    "revision": "260fcd703cef2b678db5e118bf0b5892",
+    "url": "/my-portfolio/static/media/Nders.260fcd70.png"
+  },
+  {
+    "revision": "4add67decbf734f2e72ecdb6b3f9fbf8",
+    "url": "/my-portfolio/static/media/North West Investments Security Inc.4add67de.png"
+  },
+  {
+    "revision": "504a890a4657268165a2fbad2e9729b0",
+    "url": "/my-portfolio/static/media/Ollo.504a890a.png"
+  },
+  {
+    "revision": "728248a7df1a052c8c5ae3ec43d25361",
+    "url": "/my-portfolio/static/media/Simple-Business-Website-Template-New.728248a7.png"
+  },
+  {
+    "revision": "ae646a942e2eabffb10823a0c325d272",
+    "url": "/my-portfolio/static/media/github-icon.ae646a94.png"
+  },
+  {
+    "revision": "d6016f62d9534b86266fe4e7fbe29551",
+    "url": "/my-portfolio/static/media/hero-img-mobile-screen.d6016f62.png"
+  },
+  {
+    "revision": "17d095ff824612a711bb5d791b3550b2",
+    "url": "/my-portfolio/static/media/rsz_esi-laptop.17d095ff.png"
+  },
+  {
+    "revision": "faeb4d1468e050a107aa0250fd1ee67d",
+    "url": "/my-portfolio/static/media/true-radiance.faeb4d14.png"
+  }
+]);
