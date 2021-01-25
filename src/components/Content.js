1ab31ad5d0d32bc4ec00731deadc3e1573103735
devonwintz <samuelwintz@gmail.com>
2021-01-25 02:02:25 -0800

    added projects

--- src/components/Content.js
+++ src/components/Content.js
@@ -108,7 +108,7 @@ const Content = (props) => {
             quality websites and expectional user experience.
           </span>
           <br />
-          <button className="btn mt-5" style={{ borderColor: "#1c2331" }}>
+          {/* <button className="btn mt-5" style={{ borderColor: "#1c2331" }}>
             <a
               href="https://drive.google.com/file/d/1LKIotIyjlfzhn4fiBg7wiO93Hx-IZ_dH/view?usp=sharing"
               download
@@ -117,7 +117,7 @@ const Content = (props) => {
             >
               Download My CV
             </a>
-          </button>
+          </button> */}
         </div>
         <div className="col-md-6 about-me-progress mt-3">
           <div className="skills mt-4">
